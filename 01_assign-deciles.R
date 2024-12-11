1ea4981b2b8903b7aad1cf356e39c96a7eb12bed
jeancjw <59990552+jeancjw@users.noreply.github.com>
2024-12-11 11:47:56 -0500

    Assign income deciles spatially

--- 01_assign-deciles.R
+++ 01_assign-deciles.R
@@ -2,47 +2,46 @@
 ### ASSIGN DECILES ###
 ######################
 
-# READ: This code is for assigning income deciles to raster cells.
-## We do this by province/HUC since income distributions are given by province/HUC
+## Code description: 
+### The Philippine Statistics Authority provides income distributions on the Province/Highly-Urbanised City level, categorised by income deciles.
+### To assign these incomes spatially, we first need to assign income deciles to each province/HUC (because decile 1 in province A is not the same as decile 1 in province B)
+### We use a global poverty map comprising spatially-distributed relative wealth indices (RWI) [from Chi et al., 2022] to spatially distribute income.
+### RWI were converted to rasters and clipped to each province/HUC using QGIS
+
 
 library(raster)
 library(dplyr)
 
-setwd("D:/Jeanette")
+# Read file paths
+RWI_path <- "replace with your own filepath to RWI rasters for each province/HUC"
+POP_path <- "replace with your own filepath to population rasters for each province/HUC" # we used Worldpop data
 
-#### 1. PROVINCE ####
-# File paths
-RWI_path <- "00_Data/Relative wealth index/rwi_province" # rasters converted from Chi et al. (2022)'s global poverty map/relative wealth indices
-POP_path <- "00_Data/Population/adm2_resamp" # population rasters from Worldpop
-# Read in raster files
+# Read individual raster file paths
 RWI_filenames <- list.files(path = RWI_path, pattern = '.tif$', full.names = TRUE) # relative wealth index raster file paths
 POP_filenames <- list.files(path = POP_path, pattern = '.tif$', full.names = TRUE) # population raster file paths
 
 # Check if rasters are same resolution and extent 
 # compareRaster(raster(RWI_filenames[[2]]), raster(POP_filenames[[2]]))
 
-# Extract province names from RWI filenames
+# Extract province names
 province_names <- tools::file_path_sans_ext(basename(RWI_filenames))
 
 # Path to save output
-output_path <- "00_Data/Income decile/rasters_province_exceptions/"
+output_path <- "replace with your own file path where you want to save your work"
 
-# Set the starting index 
-start_index <- 1  # To resume processing mid-way (due to errors), change this index to where you want to resume 
-
-# no. 11 Batanes is empty - no population 
 
 # Loop through each province
-for (i in start_index:length(RWI_filenames)){
+for (i in 1:length(RWI_filenames)){
   start_time <- Sys.time()
   
   cat("Processing Province:", province_names[i], "\n")
+  
   # Read RWI and POP raster
   rwi <- raster(RWI_filenames[[i]])
   pop <- raster(POP_filenames[[i]])
  
 
-  # Convert to points/data frame
+  # Convert to points
   rwi_df <- rasterToPoints(rwi)
   colnames(rwi_df) <- c("x", "y", "rwi")
   pop_df <- rasterToPoints(pop)
@@ -51,7 +50,6 @@ for (i in start_index:length(RWI_filenames)){
   
   # merge rwi and pop data frames
   data <- merge(rwi_df, pop_df, by = c("x","y"))
-
   
    # Sort combined df by RWI values in ascending order
   data <- data[order(data$rwi), ]
@@ -62,7 +60,7 @@ for (i in start_index:length(RWI_filenames)){
   # Calculate the total population
   total_pop <- sum(data$pop)
   
-  # Define no. of ppl in each decile
+  # Define no. of people in each decile (Each decile should have an equal number of people)
   decile_pop <- total_pop / 10
   
   # Calculate the decile thresholds based on equal population count
